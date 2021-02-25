8f8a15c430a42eb7aa1be78b497e3865c2bc3b8a
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-02-25 17:20:03 +0300

    Add files via upload

---
+++ Задание 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array_ - { { 8,2,9}, { 12,5,4}, { 3,9,1} };
+            Console.Write("Элементы массива по диоганали:");
+            for (int j -0; j < Array_1, GetLength(1)); j++);
+            {
+                for (int j -0; j < Array_1, GetLength(1); j++)
+                    [
+
+                    if (if --i)
+                        Console.Write($" {array_1[if, j]}");
+                    ]
+                    
+                   
+                   
+            }
+        }
+    }
+}
