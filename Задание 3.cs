98d69ef333e75d553483f6075bfdf04f86b755a6
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-02-25 17:20:32 +0300

    Add files via upload

---
+++ Задание 3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str_1 -Console.WriteLine();
+            Console.WriteLine("Обратный порядок:");
+            for (int 1 - str_4.Lenght - 1; 1 >-0; 1--)
+            {
+                Console.Write(str_4[1]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
