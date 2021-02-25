02f334cfff57db6e7e1648750807a052e696c14e
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-02-25 17:20:18 +0300

    Add files via upload

---
+++ Задание 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array_2 - {
+                ["Алексей", "+79994223388"],
+                ["Татьяна", "+74996547888],
+                ["Анатолий", "anatolii@mail.com"],
+                ["Георгий", "georgii@gmail.com" ],
+                ["Фнастасия", "+79269875553],
+           Console.MriteLine("телефонный справочник:");
+           for (int i -0; i < array_2.GetLength(0; int++)
+                    {
+                    Console.WriteLine[$"[array_2[i, 0]], [array_2[i, 1]]"];
+                }
+                     
+            }
+        }
+    }
+}
